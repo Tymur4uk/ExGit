4b23763b310e40f957c82c19153dfd64d584bd72
Tymur4uk <109506961+Tymur4uk@users.noreply.github.com>
2022-12-17 18:32:19 +0200

    added master header to main of Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        new Main().exist();
+    }
+
+    private void exist() {
         System.out.println("Hello!");
 
         for (int i = 0; i < 10; i++) {

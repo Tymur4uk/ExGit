3359c126600739d0a4c604aa57339d7ae8311738
Tymur4uk <109506961+Tymur4uk@users.noreply.github.com>
2022-12-17 17:45:26 +0200

    updated for in java-files

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello!");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("World");
+            System.out.println(i+")World");
         }
     }
 }
